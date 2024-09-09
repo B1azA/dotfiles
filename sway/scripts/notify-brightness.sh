70087a335e5957f948d9b21c2319d85f843ea9f2
Josef Blažek <blaza004@gmail.com>
2024-09-09 15:10:08 +0200

    update

--- sway/scripts/notify-brightness.sh
+++ sway/scripts/notify-brightness.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-brightness=$(xbacklight -get)
+brightness=$(cat /sys/class/backlight/intel_backlight/brightness)
+max_brightness=$(cat /sys/class/backlight/intel_backlight/max_brightness)
+brightness_percentage=$(($brightness * 100 / $max_brightness))
 
-dunstify -a "brightness" -u low -h string:x-dunst-stack-tag:brightness -h int:value:$brightness "Brightness:  $brightness%" -i display
+dunstify -a "brightness" -u low -h string:x-dunst-stack-tag:brightness -h int:value:$brightness_percentage "Brightness:  $brightness_percentage%" -i display
